04bd34fd7122019c0af7d763fb722ab7030dab6c
HammerJarl <mollinedo.pavels@gmail.com>
2025-04-25 10:07:48 -0400

    Update test examenes.py

--- test examenes.py
+++ test examenes.py
@@ -45,16 +45,61 @@ def ingresar_preguntas():
         })
         print (f"Total de preguntas ingresadas: {len(banco_preguntas)}")
     
-    def generar_examen (num_preguntas):
-        if num_preguntas > len(banco_preguntas):
-            print (f"No hay suficientes pregunas. El máximo disponible es: {len(banco_preguntas)}")
-            return None
-        if num_preguntas <=0:
-            print ("El número de preguntas debe ser mayor a 0")
-            return None
+def generar_examen(num_preguntas):
+    if num_preguntas > len(banco_preguntas):
+        print (f"No hay suficientes pregunas. El máximo disponible es: {len(banco_preguntas)}")
+        return None
+    if num_preguntas <=0:
+        print ("El número de preguntas debe ser mayor a 0")
+        return None
         
-        #seleccionar y randomizar preguntas
-        preguntas_seleccionadas = random.sample(banco_preguntas, num_preguntas)
-        examen = []
-        for i, item in enumerate(preguntas_seleccionadas, 1):
-            opciones_random = item["opciones"].copy
+    #seleccionar y randomizar preguntas
+    preguntas_seleccionadas = random.sample(banco_preguntas, num_preguntas)
+    examen = []
+    for i, item in enumerate(preguntas_seleccionadas, 1):
+        opciones_random = item["opciones"].copy()
+        random.shuffle(opciones_random) #random
+        examen.append({
+            "pregunta": f"Pregunta {i}: {item['pregunta']}",
+            "opciones": opciones_random,
+            "repuesta": chr(97 + opciones_random.index(item['opciones'][ord(item['respuesta']) - 97]))
+         })
+    return examen
+
+def mostrar_examen (examen, version):
+    print(f"\n=== EXAMEN VERSION {version} ===")
+    for item in examen:
+        print(item["pregunta"])
+        for j, opcion in enumerate (item["opciones"]):
+            print(f"{chr(97 + j)}) {opcion}")
+        print ()
+    
+#menu ususario final
+def menu_usuario():
+    if not banco_preguntas:
+        print("No hay preguntas disponibles. El administrador debe ingresarlas primero.")
+        return
+        
+    print("\n=== MODO USUARIO: GENERAR EXAMEN ===")
+    try:
+        num_preguntas = int (input("cuantas preguntas desea en el examen? (màximo 100): "))
+        if num_preguntas >100:
+            print("El limite maximo de preguntas es 100")
+            return
+        #generar 2 examenes
+        examen1 = generar_examen(num_preguntas)
+        examen2 = generar_examen(num_preguntas)
+
+        if examen1 and examen2:
+            mostrar_examen(examen1, "A")
+            mostrar_examen(examen2, "B")
+    except ValueError:
+        print("Por favor, ingresa un número valido")
+
+#main
+if __name__ == "__main__":
+    # admin ingresa preguntas
+    ingresar_preguntas()
+
+    #usuario final genera examen
+    menu_usuario()
